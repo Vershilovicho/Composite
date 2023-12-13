e6b2fa43246149b4bbe2137c1af0ec73294c5096
Vershilovicho <vernik281186@gmail.com>
2023-12-13 18:01:07 +0300

    Добавила изменения в д/з

--- src/main/java/HW_2/Cirсle.java
+++ src/main/java/HW_2/Cirсle.java
@@ -2,6 +2,7 @@ package HW_2;
 
 public class Cirсle implements Share {
         public void draw(){
+
             System.out.println("Привет, я круг.");
         }
 }
--- src/main/java/HW_2/Composite.java
+++ src/main/java/HW_2/Composite.java
@@ -16,5 +16,4 @@ public class Composite implements Share {
             component.draw();
         }
     }
-
 }
